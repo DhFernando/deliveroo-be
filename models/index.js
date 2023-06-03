c2d57944a136d8b430ac77cc36f6644686047f49
Dilshan Fernando <dilshanfe@99x.io>
2023-06-03 14:31:24 +0530

    first commit

---
+++ models/index.js
@@ -0,0 +1,36 @@
+const dbConfig = require('./../config/bdConfig')
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(
+        dbConfig.DB,
+        dbConfig.USER,
+        dbConfig.PASSWORD,{
+            host: dbConfig.HOST,
+            dialect: dbConfig.dialect,
+            operatorsAliases: false,
+            pool: {
+                max: dbConfig.pool.max,
+                min: dbConfig.pool.min,
+                acquire: dbConfig.pool.acquire,
+                idle: dbConfig.pool.idle
+            }
+        }
+    )
+
+sequelize.authenticate()
+.then(()=>{
+    console.log('connection established');
+})
+.catch(err => {
+    console.log(err)
+})
+const db = {}
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+db.products = require('./productModel.js')(sequelize, DataTypes)
+db.reviews = require('./reviewModel.js')(sequelize, DataTypes)
+
+db.sequelize.sync({ force: false })
+.then(()=>{ console.log('re-sync done!') })
+
+module.exports = db
---
+++ models/reviewModel.js
@@ -0,0 +1,12 @@
+module.exports = function(sequelize, DataTypes){
+    const Review = sequelize.define('review', { 
+        rating: {
+            type: DataTypes.INTEGER,
+
+        },
+        description: {
+            type: DataTypes.TEXT
+        }  
+    })
+    return Review
+}
