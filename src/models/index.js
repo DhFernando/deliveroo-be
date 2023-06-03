c6f335169947aefc486e28b5e10c306c646ae9f4
DhFernando <hdilshan.fernando11@gmail.com>
2023-06-03 17:15:19 +0530

    Add restaurant model

--- src/models/index.js
+++ src/models/index.js
@@ -1,36 +1,40 @@
-const dbConfig = require('../config/dbConfig')
+const dbConfig = require('../config/dbConfig');
 const { Sequelize, DataTypes } = require('sequelize');
+
 const sequelize = new Sequelize(
-        dbConfig.DB,
-        dbConfig.USER,
-        dbConfig.PASSWORD,{
-            host: dbConfig.HOST,
-            dialect: dbConfig.dialect,
-            operatorsAliases: false,
-            pool: {
-                max: dbConfig.pool.max,
-                min: dbConfig.pool.min,
-                acquire: dbConfig.pool.acquire,
-                idle: dbConfig.pool.idle
-            }
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD,
+    {
+        host: dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        operatorsAliases: false,
+        pool: {
+            max: dbConfig.pool.max,
+            min: dbConfig.pool.min,
+            acquire: dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle
         }
-    )
+    }
+);
 
 sequelize.authenticate()
-.then(()=>{
-    console.log('connection established');
-})
-.catch(err => {
-    console.log(err)
-})
-const db = {}
-db.Sequelize = Sequelize
-db.sequelize = sequelize
+    .then(()=>{
+        console.log('connection established');
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
-db.products = require('./productModel.js')(sequelize, DataTypes)
-db.reviews = require('./reviewModel.js')(sequelize, DataTypes)
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.Products = require('./productModel.js')(sequelize, DataTypes); // TODO
+db.Reviews = require('./reviewModel.js')(sequelize, DataTypes); // TODO
+db.Restaurant = require('./restaurantModel')(sequelize);
 
 db.sequelize.sync({ force: false })
-.then(()=>{ console.log('re-sync done!') })
+    .then(()=>{ console.log('re-sync done!') });
 
-module.exports = db
+module.exports = db;
